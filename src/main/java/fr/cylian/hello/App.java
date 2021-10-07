c263278d5f57b4ac8bd520ae0293fcdb6d9f393d
Cyril Pfaff <cp512@cylian.org>
2021-10-07 13:24:02 +0200

    Initial Commit

---
+++ src/main/java/fr/cylian/hello/App.java
@@ -0,0 +1,13 @@
+package fr.cylian.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Outil.hello( "World from API" );
+    }
+}
